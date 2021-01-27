9874ee055f56b791d5f3161e39b59dada91a523c
Javilingys <shkola.govno@gmail.com>
2021-01-27 17:05:17 +0700

    Delete all

--- Assets/Scripts/SO/Level.cs
+++ Assets/Scripts/SO/Level.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+[System.Serializable]
+public class Level
 {
+    public int knifeCount = 1;
 
 }
