33c7d6d2f8483dad6234ac3dd29f5805fb800333
Javilingys <shkola.govno@gmail.com>
2021-01-28 20:35:36 +0700

    10. MainMenuUI and GameMenuUI

--- Assets/Scripts/UI/Menus/MainMenu.cs
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -1,16 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using KnifeHitClone.Managers;
 
 namespace KnifeHitClone.UI
 {
     public class MainMenu : Menu<MainMenu>
     {
+        // Only number
+        [SerializeField]
+        private TextMeshProUGUI appleText;
+        // STAGE + number
+        [SerializeField]
+        private TextMeshProUGUI stageText;
+        // SCORE + number
+        [SerializeField]
+        private TextMeshProUGUI scoreText;
 
 
         protected override void Awake()
         {
             base.Awake();
+        }
+
+        private void Start()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            DataManager.Instance.Load();
+            appleText.text = DataManager.Instance.AppleCount.ToString();
+            stageText.text = $"STAGE {DataManager.Instance.MaxStage}";
+            scoreText.text = $"SCORE {DataManager.Instance.MaxScore}";
+        }
+
+        public void OnPlayPressed()
+        {
+
+        }
+
+        // open the SettingsMenu
+        public void OnSettingsPressed()
+        {
+            SettingsMenu.Open();
+        }
+
+        // quit the application
+        public override void OnBackPressed()
+        {
+            Application.Quit();
         }
     }
 }
--- Assets/Scripts/UI/Menus/SettingsMenu.cs
+++ Assets/Scripts/UI/Menus/SettingsMenu.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsMenu : MonoBehaviour
+namespace KnifeHitClone.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SettingsMenu : Menu<SettingsMenu>
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
